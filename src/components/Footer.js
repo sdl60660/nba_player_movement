2a2e61393113de5ce8f3eb0bda9fae737d3d61cb
sdl60660 <learnersd@gmail.com>
2021-01-18 19:53:17 -0500

    Filled out footer componenet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,35 @@ import React from 'react';
 const Footer = ({ githubLink  }) => {
     return (
         <div className={"footer"}>
+        <hr />
+            <div className="footer__section">
+                <p><strong>Visualization by <a target="_blank" href="https://www.samlearner.com">Sam Learner</a></strong> |&nbsp;
+                    <a target="_blank" href="mailto:learnersd@gmail.com"><img className="icon-img" src="/images/email.svg" /></a> |&nbsp;
+                    <a target="_blank" href="https://twitter.com/sam_learner"><img className="icon-img" src="/images/twitter.svg" /></a> |&nbsp;
+                    <a target="_blank" href="https://github.com/sdl60660"><img className="icon-img" src="/images/github.png" /></a>
+                </p>
+                <p>Code and data for this project lives <a target="_blank" href="https://github.com/sdl60660/letterboxd_recommendations">here</a>.</p> 
+            </div>
+
+            <div className="footer__section">
+                <p><strong>Sources</strong></p>
+                <p>
+                    All transaction data comes from <a href="https://www.basketball-reference.com/leagues/NBA_2021_transactions.html">Basketball Reference</a>&nbsp;
+                    and <a href="https://www.prosportstransactions.com/basketball/Search/SearchResults.php?Player=&Team=&BeginDate=2020-10-11&EndDate=&PlayerMovementChkBx=yes&Submit=Search">Pro Sports Transactions</a>.&nbsp;
+                    Player stats and contract data came from Basketball Reference. Player photos came from <a href="https://www.2kratings.com/">2KRatings.com</a>.
+                </p>
+            </div>
+            
+            <div className="footer__section">
+                <p><strong>Notes</strong></p>
+                <p>
+                    Exhibit 10 and two-way contracts were not included in the listed transactions to avoid some salary complexity. It is possible that there 
+                    are inaccuracies in the underlying transaction data (I've found and manually corrected a few missing or inaccurate transactions, so I know that the data&nbsp;
+                    from Basketball Reference isn't perfect).
+                </p>
+            </div>
+
+            <p>Last Updated: January 2021</p>
         </div>
     )
 }
